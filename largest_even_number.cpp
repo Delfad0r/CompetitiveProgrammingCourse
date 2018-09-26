46060933573c801ac0e1ef50f3c33f0c983fbfa0
Filippo Baroni <filippo.gianni.baroni@gmail.com>
2018-09-26 21:58:59 +0200

    Create largest_even_number.cpp

---
+++ largest_even_number.cpp
@@ -0,0 +1,32 @@
+/*
+The solution is very straightforward: we simply sort the
+string, then put the smallest even digit (if any) at the end;
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    string S;
+    cin >> S;
+    sort(S.begin(), S.end());
+    string last;
+    for(int i = 0; i < int(S.length()); ++i) {
+        if((S[i] - '0') % 2 == 0) {
+            last += S[i];
+            S.erase(i, 1);
+            break;
+        }
+    }
+    reverse(S.begin(), S.end());
+    cout << S << last << '\n';
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+}
