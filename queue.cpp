beec99b43c1b33bc51c67cbbff9899304b3ba316
Filippo Baroni <filippo.gianni.baroni@gmail.com>
2019-01-10 11:47:14 +0100

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,42 @@
+/*
+We iterate over the people sorted by increasing a[i]. If a[i]>i then it is
+impossible, otherwise we insert the i-th person at position a[i] with height
+N+1-i, so that there are exactly a[i] people taller than him before him in
+the queue.
+
+Time complexity:  O(N^2)
+Space complexity: O(N)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<string, int> SI;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int N;
+    cin >> N;
+    vector<SI> V(N);
+    for(SI& p : V) {
+        cin >> p.first >> p.second;
+    }
+    sort(V.begin(), V.end(), [](SI& x, SI& y) { return x.second < y.second; });
+    vector<SI> ans;
+    for(int n = 0; n < N; ++n) {
+        if(V[n].second > n) {
+            cout << "-1\n";
+            return 0;
+        }
+        ans.emplace(ans.begin() + V[n].second, V[n].first, N + 1 - n);
+    }
+    for(SI& p : ans) {
+        cout << p.first << " "  << p.second << "\n";
+    }
+    
+    return 0;
+}
